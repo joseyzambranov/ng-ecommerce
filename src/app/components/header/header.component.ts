0cbdd3c52191d1e13bbbbcda39bd94a3ed087200
joseyzambranov <joseyzambranovpe@gmail.com>
2022-11-06 20:32:31 -0500

    first commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit,ViewChild, ElementRef} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  flag = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  showMenu(){
+    this.flag = !this.flag;
+  }
+
+}
+
+
